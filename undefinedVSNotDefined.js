29556e8b06012d2dc1409794d06eef2d19adae8d
Dil ip <150729849+bitscurrent@users.noreply.github.com>
2024-06-22 14:40:27 +0530

    Add files via upload

---
+++ undefinedVSNotDefined.js
@@ -0,0 +1,7 @@
+// A variable is undefined if it has been declared but not assigned a value, while a variable is not defined if it has not been declared at all, resulting in a ReferenceError when accessed.
+
+var x;
+
+console.log(x); // Output: undefined
+
+// console.log(z); //ReferenceError: z is not defined
