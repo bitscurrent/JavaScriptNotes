403cbf3d45985872dd35d657c6d3ca561d3e7e4d
Dil ip <150729849+bitscurrent@users.noreply.github.com>
2024-06-25 16:21:40 +0530

    add comments

--- HighOrderFunction.js
+++ HighOrderFunction.js
@@ -1,19 +1,20 @@
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map(function (number) {
-  return number * 2;
-});
+//The map, filter, and reduce are array methods (built-in higher-order functions). 
 
-const tripled = numbers.map((number) => {
+//map
+//The map function takes a function as an argument and applies it to each element in the array. 
+const triplet = numbers.map((number) => {
   return number * 3;
 });
 
 const square = numbers.map((number) => number * number);
 
 console.log(doubled);
-console.log(tripled);
+console.log(triplet);
 console.log(square);
 
 // filter
+//The filter method also takes a function as an argument and filters out elements according to the logic provided in the function. 
+});
 const evenNumbers = numbers.filter((number) => {
   return number % 2 == 0;
 });
@@ -21,6 +22,10 @@ const evenNumbers = numbers.filter((number) => {
 console.log(evenNumbers);
 
 // reduce
+//The reduce method, on the other hand, takes two arguments inside its callback function: the accumulator and the current element.
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(function (number) {
+  return number * 2;
 const sum = numbers.reduce((cont, curr) => {
   let total = 0;
   total = cont + curr;
